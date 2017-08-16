9a4f25095c775160516c56856150e4f14802b81c
José Gabriel González <jgabrielsinner10@gmail.com>
2017-08-16 07:12:24 -0400

    It was added the ability of update and delete heroes

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -59,6 +59,14 @@ export class HeroService {
       .catch(this.handleError);
   }
 
+  delete(id: number): Promise<void> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.delete(url, {headers: this.headers})
+      .toPromise()
+      .then(() => null)
+      .catch(this.handleError);
+  }
+
   /**
    * @deprecated
    * @returns {Promise<Hero[]>}
